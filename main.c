2db5cfae6a95b7146c22c05904d08e94dd8cabe2
ska42 <skalyxe00@gmail.com>
2020-08-28 04:43:33 +0200

    client left working

--- main.c
+++ main.c
@@ -7,13 +7,88 @@
 #include <sys/select.h>
 #include <unistd.h>
 
+// EASY MODE
+
+typedef struct		s_client
+{
+	int			id;
+	int			fd;
+	struct s_client	*next;
+}					t_client;
+
 typedef struct		s_all
 {
-	int		sockfd;
-	int		fd_max;
-	fd_set	fd_read;
-	fd_set	fd_master;
+	int			sockfd;
+	int			fd_max;
+	fd_set		fd_read;
+	fd_set		fd_master;
+	t_client	*clients;
 }					t_all;
+
+int					g_id;
+
+int					add_client(t_client **client, int fd)
+{
+	t_client	*tmp;
+	t_client	*new_client;
+
+	if (!(new_client = malloc(sizeof(t_client))))
+		return (-1);
+	new_client->fd = fd;
+	new_client->id = g_id;
+	if (!*client)
+		*client = new_client;
+	else
+	{
+		tmp = *client;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new_client;
+	}
+	g_id++;
+	return (g_id - 1);
+}
+
+int					remove_client(t_client **client, int fd)
+{
+	int			id;
+	t_client	*prev;
+	t_client	*tmp;
+
+	id = -1;
+	if (*client)
+	{
+		tmp = *client;
+		prev = NULL;
+		while (tmp && tmp->fd != fd)
+		{
+			prev = tmp;
+			tmp = tmp->next;
+		}
+		if (tmp)
+			id = tmp->id;
+		if (prev && tmp && tmp->next)
+			prev->next = tmp->next;
+		if (!prev && tmp && tmp->next)
+			*client = tmp->next;
+		else
+			*client = prev;
+		free(tmp);
+	}
+	return (id);
+}
+
+int					get_id_client(t_client **client, int fd)
+{
+	t_client	*tmp;
+
+	tmp = *client;
+	while (tmp && tmp->fd != fd)
+		tmp = tmp->next;
+	if (!tmp)
+		return (-1);
+	return (tmp->id);
+}
 
 void				send_all(t_all *all, char buffer[], int len, int fd_sender)
 {
@@ -61,48 +136,61 @@ int			main(void)
 	listen(all.sockfd, 100);
 
 	////////// J'AI AJOUTE DES TRUCS ICI
+	g_id = 0;
 
 	FD_ZERO(&all.fd_read);
 	FD_ZERO(&all.fd_master);
 	FD_SET(all.sockfd, &all.fd_master);
 
 	all.fd_max = all.sockfd;
+	all.clients = NULL;
 	while (1)
 	{
 		all.fd_read = all.fd_master;
 		select(all.fd_max + 1, &all.fd_read, NULL, NULL, NULL);
 		// READ A THING DOWN THERE
 		int fd = 0;
+		char str[4096];
 		while (fd < all.fd_max + 1)
 		{
+			bzero(&str, 4096);
 			if (FD_ISSET(fd, &all.fd_read))
 			{
 				int fd_client;
 				struct sockaddr_in cli;
 				char buff[4096];
-
+				bzero(&buff, 4096);
 				if (fd == all.sockfd) // NEW CLIENT
 				{
-					printf("receive\n");
 					socklen_t len = sizeof(cli);
-
 					fd_client = accept(all.sockfd, (struct sockaddr *)&cli, &len);
 					if (fd_client > all.fd_max)
 						all.fd_max = fd_client;
 					FD_SET(fd_client, &all.fd_master);
-					char welcome[] = "someone has joined\n";
-					send_all(&all, welcome, strlen(welcome), -1);
+					int new_id = add_client(&all.clients, fd_client);
+					sprintf(str, "client %d has joined\n", new_id);
+					send_all(&all, str, strlen(str), -1);
 				}
 				else // RECV SMTHG
 				{
 					int len_recv = recv(fd, buff, 4096, 0);
-					if (len_recv <= 0) // ERROR OR NO MSG
+					if (len_recv <= 0) // ERROR OR LEAVING
 					{
+						int delete_id = remove_client(&all.clients, fd);
+						if (delete_id >= 0)
+						{
+							sprintf(str, "client %d has left\n", delete_id);
+							send_all(&all, str, strlen(str), fd);
+						}
 						close(fd);
 						FD_CLR(fd, &all.fd_master);
 					}
 					else // SEND TO ALL CLIENTS
-						send_all(&all, buff, len_recv, fd);
+					{
+						int this_id = get_id_client(&all.clients, fd);
+						sprintf(str, "client %d: %s", this_id, buff);
+						send_all(&all, str, strlen(str), fd);
+					}
 				}
 			}
 			fd++;
