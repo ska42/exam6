c30a5538b279d9e555a0efea72f353dda27cf606
SimplySka <skalyxe00@gmail.com>
2020-08-28 06:40:53 +0200

    the b0ss

---
+++ README.md
@@ -0,0 +1,65 @@
+#HOW TO MAKE IT RLY FAST GOD UR SO GOOD
+
+- make function write and exit auto
+- make a chained list of clients for setup fd/id and add/remove/search functions by fd to get id
+- global/static_id to easy increment id
+- s_all struct with:
+```
+	int			sockfd
+	int			fd_max
+	fd_set		fd_read;
+	fd_set		fd_master;
+	t_client	*clients;
+```
+- function send_all to send to everyone things cause you reuse it multiple kind
+- don't forget to bzero things reusing or using or FD_ZERO
+- be the b0ss who deserve exam6 😎
+
+#MOST IMPORTANT THINGS
+```
+	# ALREADY DONE PART
+	main
+		sockfd = socket(PF_INET, SOCK_STREAM, 0);
+		bzero(&serv_addr, sizeof(serv_addr));
+		serv_addr.sin_family = AF_INET;
+		serv_addr.sin_port = htons(port);
+		serv_addr.sin_addr.s_addr = htonl(2130706433);
+		bind(sockfd, &serv_addr, sizeof(serv_addr));
+		listen(sockfd, lots_of_fd);
+
+	# NOT DONE PART
+		FD_ZERO(fd_read);
+		FD_ZERO(fd_master);
+		FD_SET(sockfd, fd_master);
+		
+		fd_max = sockfd;
+		while INFINITY
+			fd_read = fd_master;
+			select(fd_max + 1, fd_read, NULL, NULL, NULL);
+			fd = 0;
+			while fd < fd_max + 1
+				if FD_ISSET(fd, fd_read) # FD is in fd_read
+					if fd == sockfd # new client
+						len = sizeof(struct sockaddr_in);
+						new_fd = accept(sockfd, &cli, &len);
+						if (new_fd > fd_max) # UPDATE FD_MAX
+							fd_max = new_fd;
+						FD_SET(new_fd, fd_master); # set new_fd to master to add next to fd_read
+					else
+						if recv(fd, buff, 4096) <= 0 # client left or error
+							close(fd);
+							FD_CLR(fd, &fd_master); # clear fd from fd_master
+						else # msg receive so send to everyone
+							fd2 = 0;
+							while fd2 < fd_max + 1
+								if FD_ISSET(fd2, fd_master) # if fd is in fd_master
+									if fd2 != sockfd && fd2 != fd # if fd not himself
+										send(fd1, buffer, len, 0)
+								fd2++;
+				fd++;
+							
+						
+						
+					
+		
+```
--- main.c
+++ main.c
@@ -27,6 +27,17 @@ typedef struct		s_all
 
 int					g_id;
 
+void				write_error(char *str)
+{
+	write(STDERR_FILENO, str, strlen(str));
+	exit(1);
+}
+
+void				write_thing(char *str)
+{
+	write(STDIN_FILENO, str, strlen(str));
+}
+
 int					add_client(t_client **client, int fd)
 {
 	t_client	*tmp;
@@ -105,42 +116,43 @@ void				send_all(t_all *all, char buffer[], int len, int fd_sender)
 	}
 }
 
-int			main(void)
+int			main(int argc, char *argv[])
 {
-	t_all			all;
-	struct sockaddr_in serv_addr;
+	t_all				all;
+	struct sockaddr_in	serv_addr;
+	int					ret;
+	int					port;
 
+	if (argc != 2)
+		write_error("manque un arg\n");
+	port = atoi(argv[1]);
+	if (port <= 0)
+		write_error("port de merde\n");
 	all.sockfd = socket(PF_INET, SOCK_STREAM, 0);
 	if (all.sockfd < 0)
-	{
-		printf("Socket not successful truc\n");
-		exit(1);
-	}
+		write_error("socket problem\n");
 	else
-		printf("Socket successful truc\n");
+		write_thing("Good boi socket\n");
 	bzero(&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(8081);
+	serv_addr.sin_port = htons(port);
 	serv_addr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
-	int ret;
 	ret = bind(all.sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 	if (ret < 0)
-	{
-		printf("Socket not bind truc\n");
-		exit(1);
-	}
+		write_error("bind problem\n");
 	else
-	{
-		printf("Socket bind truc\n");
-	}
+		write_thing("binded\n");
 	listen(all.sockfd, 100);
 
-	////////// J'AI AJOUTE DES TRUCS ICI
+	////////// PLEINS DE NEWS TRUCS EN DESSOUS
 	g_id = 0;
 
 	FD_ZERO(&all.fd_read);
 	FD_ZERO(&all.fd_master);
 	FD_SET(all.sockfd, &all.fd_master);
+
+	int id;
+	char str[4096];
 
 	all.fd_max = all.sockfd;
 	all.clients = NULL;
@@ -150,36 +162,36 @@ int			main(void)
 		select(all.fd_max + 1, &all.fd_read, NULL, NULL, NULL);
 		// READ A THING DOWN THERE
 		int fd = 0;
-		char str[4096];
 		while (fd < all.fd_max + 1)
 		{
 			bzero(&str, 4096);
 			if (FD_ISSET(fd, &all.fd_read))
 			{
-				int fd_client;
-				struct sockaddr_in cli;
+				struct sockaddr_in		cli;
+				socklen_t				len;
 				char buff[4096];
 				bzero(&buff, 4096);
 				if (fd == all.sockfd) // NEW CLIENT
 				{
-					socklen_t len = sizeof(cli);
-					fd_client = accept(all.sockfd, (struct sockaddr *)&cli, &len);
-					if (fd_client > all.fd_max)
-						all.fd_max = fd_client;
-					FD_SET(fd_client, &all.fd_master);
-					int new_id = add_client(&all.clients, fd_client);
-					sprintf(str, "client %d has joined\n", new_id);
+					int new_fd;
+
+					len = sizeof(cli);
+					new_fd = accept(all.sockfd, (struct sockaddr *)&cli, &len);
+					if (new_fd > all.fd_max)
+						all.fd_max = new_fd;
+					FD_SET(new_fd, &all.fd_master);
+					id = add_client(&all.clients, new_fd);
+					sprintf(str, "client %d has joined\n", id);
 					send_all(&all, str, strlen(str), -1);
 				}
 				else // RECV SMTHG
 				{
-					int len_recv = recv(fd, buff, 4096, 0);
-					if (len_recv <= 0) // ERROR OR LEAVING
+					if (recv(fd, buff, 4096, 0) <= 0) // ERROR OR LEAVING
 					{
-						int delete_id = remove_client(&all.clients, fd);
-						if (delete_id >= 0)
+						id = remove_client(&all.clients, fd);
+						if (id >= 0)
 						{
-							sprintf(str, "client %d has left\n", delete_id);
+							sprintf(str, "client %d has left\n", id);
 							send_all(&all, str, strlen(str), fd);
 						}
 						close(fd);
@@ -187,8 +199,8 @@ int			main(void)
 					}
 					else // SEND TO ALL CLIENTS
 					{
-						int this_id = get_id_client(&all.clients, fd);
-						sprintf(str, "client %d: %s", this_id, buff);
+						id = get_id_client(&all.clients, fd);
+						sprintf(str, "client %d: %s", id, buff);
 						send_all(&all, str, strlen(str), fd);
 					}
 				}
